09a57c45d3ffc68b6cbdb7ae89d48de261434033
berthamz <berthamz@gmail.com>
2018-08-28 00:57:32 -0700

    Full template

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	carousel.init({target:'slideshow-mazarino', type:'fade'});
+	carousel.init({target:'slideshow-cocina', type:'fade',autoscroll:false});
+	carousel.init({target:'slideshow-closet', type:'fade',autoscroll:false});
+});
